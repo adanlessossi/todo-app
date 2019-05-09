aae740fc1f5cf2f6780daecc1070633e1f9790ce
Bernard Adanlessossi <adanlessossi@gmail.com>
2019-05-09 08:38:06 +0200

    Added Angular Basic Authentication Service

---
+++ src/app/service/basic-authentication.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+export class AuthenticationBean {
+
+  constructor(public message: string){}
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class BasicAuthenticationService {
+
+  authenticated = false;
+  errorMessage = 'Invalid credentials';
+
+  constructor(private router: Router, private httpClient: HttpClient) { }
+
+  authenticate(username, password) {
+    console.log("Login with: " + username + " and Password: " + password);
+    console.log("Before login" + this.isUserLoggedIn());
+    if (username === 'Bernard' && password === 'Passw0rd') {
+      sessionStorage.setItem('authenticatedUser', username);
+      console.log("After login" + this.isUserLoggedIn());
+      return true;
+    }
+    return false;
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticatedUser');
+    console.log("User is: " + user);
+    return !(user === null);
+  }
+
+  executeBasicAuthentication(username, password){
+    let basicHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+
+    let header = new HttpHeaders({
+      Authorization: basicHeaderString
+    })
+    return this.httpClient.get<AuthenticationBean>('http://localhost:8080/basicauth', {headers: header});
+  }
+  
+  logout() {
+    sessionStorage.removeItem('authenticatedUser');
+  }
+}
